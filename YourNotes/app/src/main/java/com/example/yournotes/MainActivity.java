c67be0a80738e6b85cf8f1bd018cd1650a83180a
01Daniel01 <93667461+01Daniel01@users.noreply.github.com>
2023-07-01 18:08:54 +0200

    Login e Registrazione funzionanti

--- YourNotes/app/src/main/java/com/example/yournotes/MainActivity.java
+++ YourNotes/app/src/main/java/com/example/yournotes/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button button;
+    public EditText email;
+    public EditText password;
+    public Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +23,31 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.createAccountButton);
 
+        email = findViewById(R.id.editEmailLogin);
+        password = findViewById(R.id.editPasswordLogin);
+        loginButton = findViewById(R.id.accessButton);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Register.class);
                 startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Bundle bundle = getIntent().getExtras();
+                String rEmail = bundle.getString("email");
+                String rPassword = bundle.getString("password");
+
+                if (email.getText().toString().equals(rEmail) && password.getText().toString().equals(rPassword)) {
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
--- YourNotes/app/src/main/java/com/example/yournotes/Register.java
+++ YourNotes/app/src/main/java/com/example/yournotes/Register.java
@@ -6,17 +6,34 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
 
     public Button button;
+    public Button confirmButton;
+
+    public EditText rUsername;
+    public EditText rEmail;
+    public EditText rPassword;
+    public EditText rConfirmPassword;
+    public EditText rCorsoLaurea;
+
+    Bundle bundle = new Bundle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        rUsername = findViewById(R.id.editNameRegister);
+        rEmail = findViewById(R.id.editEmailRegister);
+        rPassword = findViewById(R.id.editPasswordRegister);
+        rConfirmPassword = findViewById(R.id.editConfirmPasswordRegister);
+        rCorsoLaurea = findViewById(R.id.editCdLRegister);
+
         button = (Button) findViewById(R.id.backButton1);
+        confirmButton = (Button) findViewById(R.id.confirmAccountButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +42,33 @@ public class Register extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Register.this, MainActivity.class);
+
+                final String username = rUsername.getText().toString().trim();
+                final String email = rEmail.getText().toString().trim();
+                final String password = rPassword.getText().toString().trim();
+                final String confirmPassword = rConfirmPassword.getText().toString().trim();
+                final String CorsoLaure = rCorsoLaurea.getText().toString().trim();
+
+                bundle.putString("email", email);
+                bundle.putString("password", password);
+
+                i.putExtras(bundle);
+
+                startActivity(i);
+
+            }
+        });
+
+
+
+
+
 
 
 
