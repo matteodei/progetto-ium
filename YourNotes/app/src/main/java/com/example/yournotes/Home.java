beb1b73f0207cab0625aa126b3219e394f5b20a4
01Daniel01 <93667461+01Daniel01@users.noreply.github.com>
2023-07-01 22:25:58 +0200

    test navbar

--- YourNotes/app/src/main/java/com/example/yournotes/Home.java
+++ YourNotes/app/src/main/java/com/example/yournotes/Home.java
@@ -3,12 +3,31 @@ package com.example.yournotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+import com.example.yournotes.databinding.ActivityMainBinding;
 
 public class Home extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Button test = findViewById(R.id.bottomNavigationView);
+
+        binding.
+
+
     }
 }
