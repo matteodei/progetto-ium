eaf45ea2e00c55c199e19f743a5b296b4fdf179e
01Daniel01 <93667461+01Daniel01@users.noreply.github.com>
2023-07-01 17:18:12 +0200

    id corretti e spostamento tra schermate

--- YourNotes/app/src/main/java/com/example/yournotes/MainActivity.java
+++ YourNotes/app/src/main/java/com/example/yournotes/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.yournotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.createAccountButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
--- YourNotes/app/src/main/java/com/example/yournotes/Register.java
+++ YourNotes/app/src/main/java/com/example/yournotes/Register.java
@@ -3,12 +3,30 @@ package com.example.yournotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        button = (Button) findViewById(R.id.backButton1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
