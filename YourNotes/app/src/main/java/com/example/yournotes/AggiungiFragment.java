16ca0f5f76cc6126699acf78f2560d9f3e7a9ca8
Gabriele Pinducciu <47276259+gabrielePinducciu@users.noreply.github.com>
2023-07-02 15:58:20 +0200

    file write

--- YourNotes/app/src/main/java/com/example/yournotes/AggiungiFragment.java
+++ YourNotes/app/src/main/java/com/example/yournotes/AggiungiFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +17,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AggiungiFragment extends Fragment {
+
+
+
+    Bundle bundle = new Bundle();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -49,16 +56,26 @@ public class AggiungiFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_aggiungi, container, false);
     }
+
+
 }
--- YourNotes/app/src/main/java/com/example/yournotes/Home.java
+++ YourNotes/app/src/main/java/com/example/yournotes/Home.java
@@ -11,6 +11,8 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.EditText;
+import java.io.FileWriter;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
 
 
 import com.example.yournotes.databinding.ActivityHomeBinding;
@@ -22,6 +24,13 @@ import com.example.yournotes.databinding.ActivityMainBinding;
 public class Home extends AppCompatActivity {
 
     ActivityHomeBinding binding;
+
+
+    public Button confirmButtonUpload;
+
+    public EditText uCdl;
+    public EditText uMateria;
+    public EditText uDescrizione;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,31 @@ public class Home extends AppCompatActivity {
             }
             return true;
         });
+
+        uCdl = findViewById(R.id.getCdl);
+        uMateria = findViewById(R.id.getMateria);
+        uDescrizione = findViewById(R.id.getDescrizione);
+
+        confirmButtonUpload = (Button)  findViewById(R.id.confirmAccountButton);
+/*
+        String cdl = uCdl.getText().toString().trim();
+        String materia = uMateria.getText().toString().trim();
+        String descrizione = uDescrizione.getText().toString().trim();
+
+
+
+        try {
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write(cdl+";"+materia+";"+descrizione);
+            myWriter.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+         */
+
 
 
     }
