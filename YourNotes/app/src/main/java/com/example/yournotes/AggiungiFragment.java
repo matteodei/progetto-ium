179287deae9a8a5a9ea13cf3587e10b51bb3d741
Gabriele Pinducciu <47276259+gabrielePinducciu@users.noreply.github.com>
2023-12-13 21:36:45 +0100

    modifiche aggiungi fragment
    search bar con suggerimenti in tempo reale & anno e semestre come menu a tendina e carica file pdf (wip). TODO: aggiustare gli stili delel varie cose che ho modificato

--- YourNotes/app/src/main/java/com/example/yournotes/AggiungiFragment.java
+++ YourNotes/app/src/main/java/com/example/yournotes/AggiungiFragment.java
@@ -1,7 +1,11 @@
 package com.example.yournotes;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
@@ -10,13 +14,50 @@ import android.util.LogPrinter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AggiungiFragment extends Fragment {
 
+    private static final int PICK_PDF_FILE = 1;
+    String[] arrayMaterie = new String[]{
+            "Analisi Matematica", "Architettura dei Calcolatori", "Biologia Molecolare", "Chimica",
+            "Chimica Organica", "Diritto Civile", "Diritto del Lavoro", "Economia", "Elettronica",
+            "Fisica", "Filologia Classica", "Filosofia", "Geografia Economica", "Geometria",
+            "Ingegneria del Software", "Lingua Inglese", "Letteratura Italiana", "Microeconomia",
+            "Programmazione 1", "Programmazione 2", "Psicologia", "Scienze Politiche", "Storia dell'Arte",
+            "Storia Medievale", "Teoria dei Sistemi", "Statistica", "Programmazione 2", "Analisi Matematica",
+            "Filosofia", "Geometria", "Fisica", "Chimica", "Storia dell'Arte", "Letteratura Italiana",
+            "Economia", "Ingegneria del Software", "Algoritmi e Strutture Dati", "Fisiologia Umana",
+            "Lingua Inglese", "Storia Medievale", "Diritto Civile", "Architettura dei Calcolatori",
+            "Biologia Molecolare", "Scienze Politiche", "Statistica", "Psicologia", "Geografia Economica",
+            "Chimica Organica", "Filologia Classica", "Antropologia Culturale", "Elettronica",
+            "Teoria dei Sistemi", "Microeconomia", "Diritto del Lavoro"
+    };
+
+    String[] arrayMaterieSenzaDuplicati = new String[]{
+            "Analisi Matematica", "Architettura dei Calcolatori", "Biologia Molecolare", "Chimica",
+            "Chimica Organica", "Diritto Civile", "Diritto del Lavoro", "Economia", "Elettronica",
+            "Fisica", "Filologia Classica", "Filosofia", "Geografia Economica", "Geometria",
+            "Ingegneria del Software", "Lingua Inglese", "Letteratura Italiana", "Microeconomia",
+            "Programmazione 1", "Programmazione 2", "Psicologia", "Scienze Politiche", "Storia dell'Arte",
+            "Storia Medievale", "Teoria dei Sistemi", "Statistica", "Algoritmi e Strutture Dati", "Fisiologia Umana",
+            "Antropologia Culturale"
+    };
+
+    String[] arrayAnni = new String[]{"1", "2", "3"};
+    String[] arraySemestri = new String[]{"1", "2"};
+
     private DatabaseHelper dbHelper;
+    AutoCompleteTextView autoCompleteTextView;
+    Spinner spinnerAnno;
+    Spinner spinnerSemestre;
 
     public AggiungiFragment() {
     }
@@ -31,35 +72,62 @@ public class AggiungiFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_aggiungi, container, false);
 
+        autoCompleteTextView = rootView.findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(requireContext(), android.R.layout.simple_dropdown_item_1line, arrayMaterieSenzaDuplicati);
+        autoCompleteTextView.setAdapter(adapter);
+
+        spinnerAnno = rootView.findViewById(R.id.spinnerAnno);
+        ArrayAdapter<String> adapterAnno = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, arrayAnni);
+        adapterAnno.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAnno.setAdapter(adapterAnno);
+
+        spinnerSemestre = rootView.findViewById(R.id.spinnerSemestre);
+        ArrayAdapter<String> adapterSemestre = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, arraySemestri);
+        adapterSemestre.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSemestre.setAdapter(adapterSemestre);
+
+
         dbHelper = new DatabaseHelper(requireContext());
 
         Button buttonConferma = rootView.findViewById(R.id.buttonConferma);
-        //Button buttonElimina = rootView.findViewById(R.id.buttonElimina);
-
         buttonConferma.setOnClickListener(v -> confermaInserimento());
 
+        //Button buttonElimina = rootView.findViewById(R.id.buttonElimina);
         //buttonElimina.setOnClickListener(v -> eliminaDatabase());
+
+        Button buttonSelectPDF = rootView.findViewById(R.id.buttonSelectPDF);
+
+        buttonSelectPDF.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("application/pdf");
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+            try {
+                startActivityForResult(Intent.createChooser(intent, "Seleziona un file PDF"), PICK_PDF_FILE);
+            } catch (android.content.ActivityNotFoundException ex) {
+                // Gestisci l'eccezione, se necessario
+            }
+        });
 
         return rootView;
     }
 
     private void confermaInserimento() {
 
-        // Recupera i dati dai campi di input EditText
-        EditText editTextName = requireView().findViewById(R.id.editTextName);
+        autoCompleteTextView = requireView().findViewById(R.id.autoCompleteTextView);
         EditText editTextCdL = requireView().findViewById(R.id.editTextCdL);
-        EditText editTextYear = requireView().findViewById(R.id.editTextYear);
-        EditText editTextSemester = requireView().findViewById(R.id.editTextSemester);
+        spinnerAnno = requireView().findViewById(R.id.spinnerAnno);
+        spinnerSemestre = requireView().findViewById(R.id.spinnerSemestre);
         EditText editTextCourseTopics = requireView().findViewById(R.id.editTextCourseTopics);
 
-        String nome = editTextName.getText().toString();
+        String nome = autoCompleteTextView.getText().toString();
         String corsoDiLaurea = editTextCdL.getText().toString();
-        String anno = editTextYear.getText().toString();
-        String semestre = editTextSemester.getText().toString();
+        String anno = spinnerAnno.getSelectedItem().toString();
+        String semestre = spinnerSemestre.getSelectedItem().toString();
         String argomenti = editTextCourseTopics.getText().toString();
         String username = getArguments().getString("username");
 
-        if (!nome.isEmpty() && !corsoDiLaurea.isEmpty() && !anno.isEmpty() && !semestre.isEmpty() && !argomenti.isEmpty()) {
+        if (!nome.isEmpty() && !corsoDiLaurea.isEmpty() && !argomenti.isEmpty()) {
 
             ContentValues values = new ContentValues();
             values.put(CoursesContract.COLUMN_NAME, nome);
@@ -75,17 +143,14 @@ public class AggiungiFragment extends Fragment {
 
             db.close();
 
-            editTextName.setText("");
+            autoCompleteTextView.setText("");
             editTextCdL.setText("");
-            editTextYear.setText("");
-            editTextSemester.setText("");
             editTextCourseTopics.setText("");
 
             Toast.makeText(requireContext(), "Corso inserito con successo", Toast.LENGTH_SHORT).show();
 
         } else {
             Toast.makeText(requireContext(), "Riempi tutti i campi", Toast.LENGTH_SHORT).show();
-            return;
         }
     }
 
@@ -98,4 +163,16 @@ public class AggiungiFragment extends Fragment {
         db.close();
 
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_PDF_FILE && resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            // Ora hai l'URI del file PDF selezionato, puoi fare ciò che desideri con esso
+            Log.d("--URI--", String.valueOf(uri));
+        }
+    }
+
 }
